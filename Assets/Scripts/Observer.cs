5815acb460e5ea100185bdd70e1636e3a564d0fa
Alex Summers <alexsummees1821@gmail.com>
2023-04-16 23:18:23 -0700

    Fixed transfrom bug and now have interpolation for gargoyle sight

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -9,6 +9,20 @@ public class Observer : MonoBehaviour
 
     bool m_IsPlayerInRange;
 
+    float max_look_time = 1f;
+    float current_look_time = 0;
+    bool looking_right = true;
+
+    float start_rotY;
+
+
+    void Start()
+    {
+        start_rotY = transform.parent.eulerAngles.y;
+    }
+
+
+
     void OnTriggerEnter(Collider other)
     {
         if (other.transform == player)
@@ -27,6 +41,31 @@ public class Observer : MonoBehaviour
 
     void Update()
     {
+
+        string tag = gameObject.tag;
+
+        if (tag == "Gargoyle")
+        {
+            float t;
+            current_look_time += Time.deltaTime;
+
+            if (current_look_time > max_look_time)
+            {
+                current_look_time = 0;
+                looking_right = !looking_right;
+            }
+            t = current_look_time / max_look_time;
+
+            if (looking_right)
+            {
+                transform.rotation = Quaternion.Lerp(Quaternion.Euler(0, start_rotY + 50, 0), Quaternion.Euler(0, start_rotY - 50, 0), t);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Lerp(Quaternion.Euler(0, start_rotY - 50, 0), Quaternion.Euler(0, start_rotY + 50, 0), t);
+            }
+        }
+
         if (m_IsPlayerInRange)
         {
             Vector3 direction = player.position - transform.position + Vector3.up;
